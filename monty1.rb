6a5d75d4af1bcdcd97aef9e1f99977b69ac4ddec
unknown <mellezwerver@outlook.com>
2014-11-18 12:39:06 +0100

    added comments

--- monty1.rb
+++ monty1.rb
@@ -1,10 +1,14 @@
+#safe the goats and the car in an array and shuffle them
 doors = ['car', 'goat', 'goat'].shuffle
 
+#ask the use to choose a door and safe their choice in a variable
 puts "Choose a door (1-3)"
 user_choice = gets.chomp.to_i-1
 
+#confirm their selection
 puts "You chose door "+(user_choice+1).to_s+"!"
 
+#let the program choose a door that has a goat behind it and one that isn't the same as the user's choice
 quizmaster_choice = nil
 doors.each_with_index do |door, index|
 	if index != user_choice && door == 'goat'
@@ -12,16 +16,19 @@ doors.each_with_index do |door, index|
 	end
 end
 
+#confirm the quizmaster's selection and ask the user if they want to change to the other door and save their choice in a variable
 puts "There's a goat behind door "+(quizmaster_choice+1).to_s + '.'
 puts "Do you want to change to the other door? (yes/no)"
 swap = gets.chomp
 
+#check if the user has won a car or a goat
 if swap == 'yes' && doors[user_choice] == 'goat'
 	won = 'car'
 else
 	won = 'goat'
 end
 
+#tell the user they have won the car or a goat
 puts 'You have won'.ljust(20, '.')
 puts 'a'.center(20, '.')
 puts won.to_s.ljust(20, '!')
--- monty2.rb
+++ monty2.rb
@@ -1,34 +1,53 @@
+#the amount of games to be played
 x = 1000000
+
+#the initial number of won games 
 wins = 0
 
+#this method defines the game mechanics
 def choice(swap)
+	#safe the goats and the car in an array and shuffle them
 	doors = ['car', 'goat', 'goat'].shuffle
 
+	#let the user automagically choose a random door
 	user_choice = rand(0..2)
+
+	#let the program choose a door that has a goat behind it and one that isn't the same as the user's choice
 	doors.each_with_index do |door, index|
 		if index != user_choice && door == 'goat'
 			quizmaster_choice = index
 		end
 	end
 
-	if (swap == 'yes' && doors[user_choice] == 'goat') || (swap == 'no' && doors[user_choice] == 'car')
+	#check if the user has won or not
+	if (swap == true && doors[user_choice] == 'goat') || (swap == false && doors[user_choice] == 'car')
 		return 1
 	end
 	return 0
 end
 
+#execute the choice method an x number of times
 x.times do
-	wins += choice('yes')
+	#add the value returned from the choice method to the amount of won games when always swapping doors
+	wins += choice(true)
 end
+
+#tell the user the amount of times won and lost
 puts 'Wel wisselen:'
 puts 'Gewonnen: ' + wins.to_s
 puts 'Verloren: ' + (x-wins).to_s
 puts
 
+#reset the amount of won games
 wins = 0
+
+#execute the choice method an x number of times
 x.times do
-	wins += choice('no')
+	#add the value returned from the choice method to the amount of won games when never swapping doors
+	wins += choice(false)
 end
+
+#tell the user the amount of times won and lost
 puts'Niet wisselen:'
 puts 'Gewonnen: ' + wins.to_s
 puts 'Verloren: ' + (x-wins).to_s
