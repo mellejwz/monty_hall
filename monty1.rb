9801e77d99da4bb6c2b3c3975b92a5225d4e40c5
unknown <mellezwerver@outlook.com>
2014-11-18 10:43:38 +0100

    optimized monty1.rb

--- monty1.rb
+++ monty1.rb
@@ -1,45 +1,27 @@
-deuren = ['a', 'b', 'c']
-gekozen = []
-goede_deur = deuren[rand(0..2)]
+deuren = ['auto', 'geit', 'geit'].shuffle
 
-gekozen.push goede_deur
-deuren.delete(goede_deur)
+puts "Kies een deur (1 t/m 3)"
+keuze = gets.chomp.to_i-1
 
-puts 'kies een deur (a, b of c) '
-keuze = gets.chomp
-gekozen.push keuze
-deuren.delete(keuze)
-puts
+puts "U heeft deur "+(keuze+1).to_s+" gekozen!"
 
-if keuze == goede_deur
-	mijn_keuze = deuren[rand(0..1)]
-	puts 'jij kiest voor ' + keuze + ', dan kies ik voor ' + mijn_keuze
-	deuren.delete(mijn_keuze)
-	puts
-	puts 'dan blijft deur ' + deuren[0] + ' over'
-	puts
-	puts 'wil je wisselen naar deur ' + deuren[0] + '? (ja/nee)'
-	swap = gets.chomp
-
-	if swap == 'ja'
-		puts 'fout'
-	elsif swap == 'nee'
-		puts 'goed'
-	end
-
-elsif keuze != goede_deur
-	mijn_keuze = deuren[0]
-	puts 'jij kiest voor ' + keuze + ', dan kies ik voor ' + mijn_keuze
-	deuren.delete(mijn_keuze)
-	puts
-	puts 'dan blijft deur ' + gekozen[0] + ' over'
-	puts
-	puts 'wil je wisselen naar deur ' + gekozen[0] + '? (ja/nee)'
-	swap = gets.chomp
-
-	if swap == 'nee'
-		puts 'fout'
-	elsif swap == 'ja'
-		puts 'goed'
+keuze_quizmaster = nil
+deuren.each_with_index do |deur, index|
+	if index != keuze && deur == 'geit'
+		keuze_quizmaster = index
 	end
 end
+
+puts "Kijk eens aan, achter deur "+(keuze_quizmaster+1).to_s+" zit een geit"
+puts "Wilt u wisselen?"
+wissel = gets.chomp
+
+if wissel == 'ja' && deuren[keuze] == 'geit'
+	gewonnen = 'auto'
+else
+	gewonnen = 'geit'
+end
+
+puts 'U heeft gewonnen...'
+puts '...een...'
+puts gewonnen.to_s + '!!!!!!'
