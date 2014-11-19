174e54a47b28d316c69b118a69258c39c3ebae2b
unknown <mellezwerver@outlook.com>
2014-11-19 11:36:37 +0100

    optimized the code and the program now shows the percentage of won games instead of the number of games

--- monty2.rb
+++ monty2.rb
@@ -1,53 +1,24 @@
-#the amount of games to be played
 x = 1000000
-
-#the initial number of won games 
-wins = 0
-
-#this method defines the game mechanics
+swapped_win = 0
+not_swapped_win = 0
 def choice(swap)
-	#save the goats and the car in an array and shuffle them
 	doors = ['car', 'goat', 'goat'].shuffle
-
-	#let the user automagically choose a random door
 	user_choice = rand(0..2)
-
-	#let the program choose a door that has a goat behind it and one that isn't the same as the user's choice
-	doors.each_with_index do |door, index|
-		if index != user_choice && door == 'goat'
-			quizmaster_choice = index
+	doors.each_with_index do |door, number|
+		if number != user_choice && door == 'goat'
+			quizmaster_choice = number
 		end
 	end
-
-	#check if the user has won or not
 	if (swap == true && doors[user_choice] == 'goat') || (swap == false && doors[user_choice] == 'car')
 		return 1
 	end
 	return 0
 end
-
-#execute the choice method an x number of times
 x.times do
-	#add the value returned from the choice method to the amount of won games when always swapping doors
-	wins += choice(true)
+	swapped_win += choice(true)
+	not_swapped_win += choice(false)
 end
-
-#tell the user the amount of won and lost games when always swapping
 puts 'Swapped:'
-puts 'Won games: ' + wins.to_s
-puts 'Lost games: ' + (x-wins).to_s
-puts
-
-#reset the amount of won games
-wins = 0
-
-#execute the choice method an x number of times
-x.times do
-	#add the value returned from the choice method to the amount of won games when never swapping doors
-	wins += choice(false)
-end
-
-#tell the user the amount of won and lost games when never swapping doors
-puts'Not swapped:'
-puts 'Won games: ' + wins.to_s
-puts 'Lost games: ' + (x-wins).to_s
+puts 'Won games: ' + ((((100/x.to_f)*swapped_win)*100).to_i.to_f/100).to_s + '%'
+puts 'Not swapped:'
+puts 'Won games: ' + ((((100/x.to_f)*not_swapped_win)*100).to_i.to_f/100).to_s + '%'
